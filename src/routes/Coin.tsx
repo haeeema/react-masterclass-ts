ce4991d85a24a737a9bc7ab52a4ed576946c7314
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-04-19 15:51:57 +0900

    coin tracker1

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,50 @@
+import { useLocation, useParams } from "react-router-dom";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface RouteParmas {
+  coinId: string;
+}
+
+interface RouteState {
+  name: string;
+}
+
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParmas>();
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading.."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
+}
+
+export default Coin;
